1ca09946935a8a8b1d0f7c7aeee291df050c75fb
Tom White <eugene.white@gmail.com>
2019-09-04 17:00:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
  
       item_name = req.path.split("/items/").last 
       if item = @@items.find{|i| i.name == item_name}
-        resp.write "#{item.price}"
+        resp.write item.price
       else
         resp.write "Item not found"
         resp.status = 400
